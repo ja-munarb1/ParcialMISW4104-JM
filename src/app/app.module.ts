ec042803f8873983d9d4ea72206de380da6812ce
JuanMunar <ja.munarb1@uniandes.edu.co>
2025-04-26 13:54:15 -0500

    Agregar módulo CafeModule y HttpClientModule al módulo principal de la aplicación.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module'; // ESTO SI VA?
 import { AppComponent } from './app.component';
+import { CafeModule } from './cafe/cafe.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    CafeModule,
     AppRoutingModule
   ],
   providers: [],
